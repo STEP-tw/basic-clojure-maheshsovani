ef5c714683ce14f4392e70bd544a1540fb716f59
Mahesh Sovani <in-maheshsn@maheshsovani-2.local>
2020-01-28 10:16:26 +0530

    [Mahesh] Added functions and associated simple tests for conditions.

--- src/assignments/conditions.clj
+++ src/assignments/conditions.clj
@@ -4,31 +4,31 @@
   "Returns the result of x/y unless y is 0. Returns nil when y is 0"
   {:level        :easy
    :use          '[when-not zero?]
-   :implemented? false}
-  [x y])
+   :implemented? true}
+  [x y] (when-not (zero? y) (/ x y)))
 
 (defn informative-divide
   "Returns the result of x/y unless y is 0. Returns :infinite when y is 0"
   {:level        :easy
    :use          '[if-not zero?]
-   :implemented? false}
-  [x y])
+   :implemented? true}
+  [x y] (if-not (zero? y) (/ x y) :infinite))
 
 (defn harishchandra
   "Only returns truthy values as themselves.
   Falsy values(false and nil) return nil"
   {:level        :easy
    :use          '[when-let]
-   :implemented? false}
-  [x])
+   :implemented? true}
+  [x] (when-let [x x] x))
 
 (defn yudishtira
   "Only returns truthy values as themselves.
   Falsy values(false and nil) return :ashwathama"
   {:level        :easy
    :use          '[if-let]
-   :implemented? false}
-  [x])
+   :implemented? true}
+  [x] (if-let [x x] x :ashwathama))
 
 (defn duplicate-first
   "Returns coll with the first element duplicated.
@@ -36,8 +36,8 @@
   {:level        :easy
    :use          '[when-first concat]
    :alternates   '[empty? seq? conj into]
-   :implemented? false}
-  [coll])
+   :implemented? true}
+  [coll] (when-first [x coll] (cons x coll)))
 
 (defn five-point-someone
   "Returns :chetan-bhagat if y is 5.
@@ -46,8 +46,12 @@
   Otherwise it returns :universe"
   {:level        :easy
    :use          '[cond]
-   :implemented? false}
-  [x y])
+   :implemented? true}
+  [x y] (cond
+          (= y 5) :chetan-bhagat
+          (= x 5) :satan-bhagat
+          (> x y) :greece
+          :else :universe))
 
 (defn conditions-apply
   "Given a collection of any length, returns:
@@ -69,7 +73,9 @@
   {:level        :medium
    :use          '[cond->> concat take]
    :implemented? false}
-  [coll rep? truncate? n])
+  [coll rep? truncate? n] (cond->> coll
+                                   rep? (concat (flatten (repeat coll)))
+                                   (and n truncate?) (take n)))
 
 (defn order-in-words
   "Given x, y and z, returns a vector consisting of
@@ -80,7 +86,11 @@
   {:level        :easy
    :use          '[cond-> conj]
    :implemented? false}
-  [x y z])
+  [x y z] (cond-> []
+                  (> x y) (conj :x-greater-than-y)
+                  (> y z) (conj :y-greater-than-z)
+                  (> z x) (conj :z-greater-than-x))
+  )
 
 (defn zero-aliases
   "Given a zero-like value(0,[],(),#{},{}) should
@@ -95,14 +105,24 @@
   {:level        :easy
    :use          '[case]
    :implemented? false}
-  [zero-like-value])
+  [zero-like-value] (case zero-like-value
+                      0 :zero
+                      [] :empty
+                      #{} :empty-set
+                      {} :empty-map
+                      "" :empty-string
+                      :not-zero
+                      )
+  )
 
 (defn zero-separated-palindrome
   "Given a sequence of numbers, increment the list
   and prepend a 0 to the incremented list concatenated
   with the reverse of the incremented list
   [1 2 3] -> (4 3 2 0 2 3 4)"
-  {:level :easy
-   :use '[as-> reverse]
+  {:level        :easy
+   :use          '[as-> reverse]
    :implemented? false}
-  [coll])
+  [coll]
+  (as-> coll result (map inc result) (concat result (list 0) (reverse result)))
+  )
--- test/assignments/conditions_test.clj
+++ test/assignments/conditions_test.clj
@@ -7,3 +7,73 @@
     (is (= 2 (safe-divide 4 2))))
   (testing "zero denominator"
     (is (nil? (safe-divide 3 0)))))
+
+(deftest informative-division
+  (testing "non zero denominator"
+    (is (= 2 (informative-divide 4 2))))
+  (testing "zero denominator"
+    (is (= :infinite (informative-divide 3 0)))))
+
+(deftest harishchandra-function
+  (testing "true value"
+    (is (= 2 (harishchandra 2))))
+  (testing "false value"
+    (is (= nil (harishchandra nil)))))
+
+(deftest yudishtira-function
+  (testing "true value"
+    (is (= 2 (yudishtira 2))))
+  (testing "false value"
+    (is (= :ashwathama (yudishtira false)))))
+
+(deftest duplicate-first-func
+  (testing "when collection is empty"
+    (is (= nil (duplicate-first []))))
+  (testing "when collection is not empty"
+    (is (= [1 1 2 3] (duplicate-first [1 2 3])))))
+
+
+(deftest five-point-someone-func
+  (testing "when y is 5"
+    (is (= :chetan-bhagat (five-point-someone 3 5))))
+  (testing "when x is 5"
+    (is (= :satan-bhagat (five-point-someone 5 3))))
+  (testing "when x is greater than y"
+    (is (= :greece (five-point-someone 7 6))))
+  (testing "when any of conditions does not match"
+    (is (= :universe (five-point-someone 10 10)))))
+
+
+(deftest order-in-words-func
+  (testing "when x is y and y is greater than z"
+    (is (= [:x-greater-than-y :y-greater-than-z] (order-in-words 4 3 2))))
+  (testing "when x is y and z is greater than x"
+    (is (= [:x-greater-than-y :z-greater-than-x] (order-in-words 4 3 5))))
+  (testing "when z is greater than x"
+    (is (= [:z-greater-than-x] (order-in-words 2 3 4))))
+  )
+
+(deftest repeat-and-truncate-func
+  (testing "should return empty sequence when given empty list"
+    (is (= '() (repeat-and-truncate '() true true 0))))
+  (testing "repeat and truncate"
+    (is (= '(0 1 2 3 0 1) (repeat-and-truncate (range 4) true true 6))))
+  )
+
+(deftest zero-separated-palindrome-func
+  (testing "when collection is provided"
+    (is (= '(4 3 2 0 2 3 4) (zero-separated-palindrome [1 2 3])))))
+
+(deftest zero-aliases-func
+  (testing "when zero is provided"
+    (is (= :zero (zero-aliases 0))))
+  (testing "when [] is provided"
+    (is (= :empty (zero-aliases []))))
+  (testing "when {} is provided"
+    (is (= :empty-map (zero-aliases {}))))
+  (testing "when empty string is provided"
+    (is (= :empty-string (zero-aliases ""))))
+  (testing "when non zero value is provided"
+    (is (= :not-zero (zero-aliases 1))))
+  (testing "when #{} is provided"
+    (is (= :empty-set (zero-aliases #{})))))
